61d6fa1945ed549ec8f423f938b2e421c72fc89d
SarbjeetRandhawa <sarbjeetrandhawa081@gmail.com>
2026-02-06 18:00:47 +0530

    Destructors

---
+++ destructors/destructors/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Destructors
+{
+    class Demo
+    {
+        public Demo()
+        {
+            Console.WriteLine("Constructor Invoked");
+        }
+        ~Demo()
+        {
+            Console.WriteLine("Destructor invoked");
+        }
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            Demo? d = new();
+            d = null;
+            GC.Collect();
+        }
+    }
+}
