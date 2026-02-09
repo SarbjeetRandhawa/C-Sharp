8508647809d5efc82cff8bde633e63cf393f16cc
SarbjeetRandhawa <sarbjeetrandhawa081@gmail.com>
2026-02-09 11:29:31 +0530

    Enums

---
+++ Enums/Enum-methods/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace Enums
+{
+    class Program
+    {
+        public static void Main()
+        {
+            string[] methodss = (string[])Enum.GetNames(typeof(Gender));
+
+            foreach(var value in methodss)
+            {
+                Console.WriteLine(value);
+            }
+
+            Console.WriteLine();
+
+            int[] valuee = (int[])Enum.GetValues(typeof(Gender));
+            foreach (var value in valuee)
+            {
+                Console.WriteLine(value);
+            }
+
+        }
+    }
+
+    public enum Gender
+    {
+        Unknown = 0,
+        Male = 1,
+        Female = 2
+    }
+}
---
+++ Enums/Enums/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.InteropServices.Marshalling;
+namespace Enums
+{
+
+    public class Employee
+    {
+        public string Name { get; set; }
+        public int Gender { get; set; }
+    }
+
+    public enum Gender
+    {
+        unknown,
+            male,
+            female
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            List<Employee> Emp = new List<Employee>
+            {
+                new Employee(){Name = "harsh" , Gender = 0 },
+                new Employee(){Name = "sarb" , Gender = 1 },
+                new Employee(){Name = "karan" , Gender = 2 },
+
+
+
+            };
+            foreach(var emp in Emp)
+            {
+                Console.WriteLine($"naem is {emp.Name} ,Gender is {GetGender(emp.Gender)}  ");
+            }
+        }
+        public static string GetGender(int a)
+        {
+            switch (a)
+            {
+                case (int)Gender.unknown:
+                    return "unknown";
+                case (int)Gender.male:
+                    return "Male";
+                case (int)Gender.female:
+                    return "Female";
+                default:
+                    return "invalid data";
+            }
+        }
+        
+    }
+}
