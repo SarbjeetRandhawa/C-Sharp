ae12d4a7c715e3fdf86cd9a69ef106b9b83fccf9
SarbjeetRandhawa <sarbjeetrandhawa081@gmail.com>
2026-02-10 14:23:22 +0530

    generic class

---
+++ Generic class/Generic-class/Program.cs
@@ -0,0 +1,76 @@
+﻿//using System;
+//namespace Generic
+//{
+//   public  class Typegeneric<T>
+//    {
+//        private T[] array;
+
+//        public Typegeneric(int size)
+//        {
+//            array = new T[size + 1];
+//        }
+//        public T getvalue(int index)
+//        {
+//            return array[index];
+//        }
+//        public void setvalue(T value , int index)
+//        {
+//            array[index] = value;
+//        }
+//    }
+//    public class Program
+//    {
+//        public static void Main()
+//        {
+//            Typegeneric<int> t = new(5);
+
+//            for(int i = 0; i < 5; i++)
+//            {
+//                t.setvalue(i * 5, i);
+//            }
+//            for(int i = 0; i <  5; i++)
+//            {
+//               Console.WriteLine($"{t.getvalue(i)}") ;
+//            }
+//        }
+//    }
+//}
+
+using System;
+namespace Generic
+{
+    public class Typegeneric<T>
+    {
+        private T[] array;
+
+        public Typegeneric(int size)
+        {
+            array = new T[size + 1];
+        }
+        public T getvalue(int index)
+        {
+            return array[index];
+        }
+        public void setvalue(T value, int index)
+        {
+            array[index] = value;
+        }
+    }
+    public class Program
+    {
+        public static void Main()
+        {
+            Typegeneric<char> t = new(5);
+
+            
+            for(int i = 0; i < 5; i++)
+            {
+                t.setvalue((char)(97 + i), i);
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                t.getvalue(i);
+            }
+        }
+    }
+}
