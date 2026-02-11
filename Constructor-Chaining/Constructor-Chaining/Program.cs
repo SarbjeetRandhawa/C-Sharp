51fd75ca07d6d8a76b365066d1c0cb62c416dcb2
SarbjeetRandhawa <sarbjeetrandhawa081@gmail.com>
2026-02-11 18:30:47 +0530

    Constructor Chaining

---
+++ Constructor-Chaining/Constructor-Chaining/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace ConstructorChaining
+{
+    public class Employee
+    {
+        private int id;
+        private string name;
+
+        public Employee(int id)
+        {
+            this.id = id;
+        }
+
+        public Employee(int id ,string name) : this(id)
+        {
+            this.name = name;
+        }
+        public void display()
+        {
+            Console.WriteLine($"{id} , {name}");
+        }
+    }
+
+    class Program
+    {
+        public static void Main()
+        {
+            Employee e = new(10, "Sarbjeet");
+            e.display();
+        }
+    }
+}
