882ed57634f6ae782753a02dddb9d5ef80cff016
SarbjeetRandhawa <sarbjeetrandhawa081@gmail.com>
2026-02-12 11:20:36 +0530

    IEnumerable and IQueryable

---
+++ IEnumerable/IEneumerable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ienumerable
+{
+    class Program
+    {
+        public static void Main()
+        {
+            List<int> l = new List<int>{ 1, 2, 5, 7, 8, 9 };
+
+            IEnumerable<int> evennum = l.Where(e => e % 2 == 0);
+
+            foreach(var i in evennum)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ IEnumerable/IQuerable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace Iquerable
+{
+    class Db : DbContext
+    {
+        public DbSet<User> users { get; set; }
+    }
+
+    class User
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            using var db = new Db();
+
+            IQueryable<User> q = db.users.Where(e => e.name.StartsWith('A'));
+
+            foreach(var i in q)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    } 
+}
